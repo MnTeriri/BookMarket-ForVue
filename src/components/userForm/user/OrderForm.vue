eebdceae5e5189d3461ccd109b5169e9ce57bff6
Teriri <lsy1511736399@outlook.com>
2023-07-31 23:59:43 +0800

    用户订单界面

--- src/components/userForm/user/OrderForm.vue
+++ src/components/userForm/user/OrderForm.vue
@@ -4,7 +4,7 @@
             <HeaderForm/>
             <SideMenu :orderFilter="data.orderFilter"/>
             &nbsp;&nbsp;
-            <div class="col-md-9 " style="background: #f1f1f1;">
+            <div class="col-md-9" style="background: #f1f1f1;">
                 <div style="margin: 20px">
                     <div style="height: 20px"></div>
                     <h4 style="margin-bottom: 20px">&nbsp;&nbsp;我的订单</h4>
@@ -20,9 +20,8 @@
                         </div>
                         <div class="col-4">
                             <div class="d-flex">
-                                <input class="form-control me-2" type="search" placeholder="输入订单号搜索"
-                                       aria-label="输入订单号搜索">
-                                <button class="btn btn-secondary">
+                                <input class="form-control me-2" type="search" placeholder="输入订单号搜索" aria-label="输入订单号搜索" v-model="data.searchOid">
+                                <button class="btn btn-secondary" @click="getOrderList(data.searchOid,1)">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                                          class="bi bi-search" viewBox="0 0 16 16">
                                         <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
@@ -37,20 +36,26 @@
                                 <div class="row" style="height: 70px;margin: 10px">
                                     <a style="font-size: 30px;color: #FF952F">{{ data.orderStatus[order.status] }}</a>
                                     <div class="col-9">
-                                        <a style="font-size: 10px;color: #797979">创建时间：{{order.createdTime}}</a>&nbsp;
-                                        <a style="font-size: 10px;color: #797979">收货人：{{ order.address.receiverName }}</a>&nbsp;
+                                        <a style="font-size: 10px;color: #797979">创建时间：{{ order.createdTime }}</a>&nbsp;
+                                        <a style="font-size: 10px;color: #797979">收货人：{{
+                                                order.address.receiverName
+                                            }}</a>&nbsp;
                                         <a style="font-size: 10px;color: #797979">订单号：{{ order.oid }}</a>
                                     </div>
                                     <div class="col-3 text-end">
-                                        <p style="font-size: 10px;color: #797979">实付金额：<a style="font-size: 20px;color: black">{{ order.price }}</a>元</p>
+                                        <p style="font-size: 10px;color: #797979">实付金额：<a
+                                                style="font-size: 20px;color: black">{{ order.price }}</a>元</p>
                                     </div>
                                 </div>
                             </div>
                             <div class="container border bg-light mb-3">
                                 <div class="row">
                                     <div class="col-8">
-                                        <div v-for="orderBook in order.books" :key="orderBook.id" class="row-cols-3" style="height: 100px;margin: 15px">
-                                            <img style="height: 90px;width: 90px" :src="'data:image/png;base64,'+orderBook.book.imageString" class="order-img">
+                                        <div v-for="orderBook in order.books" :key="orderBook.id" class="row-cols-3"
+                                             style="height: 100px;margin: 15px">
+                                            <img style="height: 90px;width: 90px"
+                                                 :src="'data:image/png;base64,'+orderBook.book.imageString"
+                                                 class="order-img">
                                             <a>{{ orderBook.book.bname }}</a>
                                             <a>{{ orderBook.price }}×{{ orderBook.count }}</a>
                                         </div>
@@ -58,19 +63,31 @@
                                     <div class="col-4 text-center" style="padding-top: 10px">
                                         <div class="btn-group-vertical" role="group" aria-label="Vertical button group">
                                             <template v-if="order.status===0">
-                                                <button type="button" class="btn btn-outline-success" onclick="payOrder('${order.oid}')">立即付款</button>
-                                                <button type="button" class="btn btn-outline-danger" onclick="cancelOrder('${order.oid}')">取消订单</button>
+                                                <button type="button" class="btn btn-outline-success"
+                                                        onclick="payOrder('${order.oid}')">立即付款
+                                                </button>
+                                                <button type="button" class="btn btn-outline-danger"
+                                                        onclick="cancelOrder('${order.oid}')">取消订单
+                                                </button>
                                             </template>
                                             <template v-else-if="order.status===1">
-                                                <button type="button" class="btn btn-outline-danger" onclick="cancelOrder('${order.oid}')">取消订单</button>
+                                                <button type="button" class="btn btn-outline-danger"
+                                                        onclick="cancelOrder('${order.oid}')">取消订单
+                                                </button>
                                             </template>
                                             <template v-else-if="order.status===2">
-                                                <button type="button" class="btn btn-outline-success" onclick="confirmOrder('${order.oid}')">确认收货</button>
+                                                <button type="button" class="btn btn-outline-success"
+                                                        onclick="confirmOrder('${order.oid}')">确认收货
+                                                </button>
                                             </template>
                                             <template v-else-if="order.status===3">
-                                                <button type="button" class="btn btn-outline-danger" onclick="returnGoods('${order.oid}')">申请退货</button>
+                                                <button type="button" class="btn btn-outline-danger"
+                                                        onclick="returnGoods('${order.oid}')">申请退货
+                                                </button>
                                             </template>
-                                            <button type="button" class="btn btn-outline-warning" onclick="showOrderDetail(${order.toJSONString()})">订单详情</button>
+                                            <button type="button" class="btn btn-outline-warning"
+                                                    onclick="showOrderDetail(${order.toJSONString()})">订单详情
+                                            </button>
                                         </div>
                                     </div>
                                 </div>
@@ -79,6 +96,76 @@
                     </template>
                     <div v-if="data.orderList.length===0" class="row text-center" style="height: 200px;">
                         <h6>当前没有订单！</h6>
+                    </div>
+                    <div v-if="data.totalPage!==0" class="d-flex justify-content-center" style="padding: 20px">
+                        <nav>
+                            <ul class="pagination">
+                                <li class="page-item">
+                                    <a class="page-link" href="#" aria-label="Previous">
+                                        <span aria-hidden="true">&laquo;</span>
+                                    </a>
+                                </li>
+                                <template v-if="data.totalPage<=7"><!--总页数小于等于7-->
+                                    <template v-for="item in data.totalPage" :key="item">
+                                        <li class="page-item" :class="{'active':data.selectedPage===item}">
+                                            <span class="page-link" @click="getOrderList(data.searchOid,item)">{{ item }}</span>
+                                        </li>
+                                    </template>
+                                </template>
+                                <template v-else><!--总页数大于7-->
+                                    <template v-if="data.selectedPage<=4"><!--当已选择页数小于等于4时-->
+                                        <template v-for="item in 5" :key="item">
+                                            <li class="page-item" :class="{'active':data.selectedPage===item}">
+                                                <span class="page-link" @click="getOrderList(data.searchOid,item)">{{ item }}</span>
+                                            </li>
+                                        </template>
+                                        <li class="page-item disabled">
+                                            <span class="page-link">...</span>
+                                        </li>
+                                        <li class="page-item">
+                                            <span class="page-link" @click="getOrderList(data.searchOid,data.totalPage)">{{data.totalPage}}</span>
+                                        </li>
+                                    </template>
+                                    <template v-else-if="data.selectedPage<=data.totalPage-4"><!--当已选择页数小于等于总页数-4时-->
+                                        <li class="page-item">
+                                            <span class="page-link" @click="getOrderList(data.searchOid,1)">1</span>
+                                        </li>
+                                        <li class="page-item disabled">
+                                            <span class="page-link">...</span>
+                                        </li>
+                                        <template v-for="item in 3" :key="item">
+                                            <li class="page-item" :class="{'active':item===2}">
+                                                <span class="page-link" @click="getOrderList(data.searchOid,data.selectedPage-2+item)">{{data.selectedPage - 2 + item}}</span>
+                                            </li>
+                                        </template>
+                                        <li class="page-item disabled">
+                                            <span class="page-link">...</span>
+                                        </li>
+                                        <li class="page-item">
+                                            <span class="page-link" @click="getOrderList(data.searchOid,data.totalPage)">{{data.totalPage}}</span>
+                                        </li>
+                                    </template>
+                                    <template v-else><!--当已选择页数大于总页数-4时-->
+                                        <li class="page-item">
+                                            <span class="page-link" @click="getOrderList(data.searchOid,1)">1</span>
+                                        </li>
+                                        <li class="page-item disabled">
+                                            <span class="page-link">...</span>
+                                        </li>
+                                        <template v-for="item in 5" :key="item">
+                                            <li class="page-item" :class="{'active':data.totalPage-5+item===data.selectedPage}">
+                                                <span class="page-link" @click="getOrderList(data.searchOid,data.totalPage-5+item)">{{data.totalPage - 5 + item}}</span>
+                                            </li>
+                                        </template>
+                                    </template>
+                                </template>
+                                <li class="page-item">
+                                    <a class="page-link" href="#" aria-label="Next">
+                                        <span aria-hidden="true">&raquo;</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </nav>
                     </div>
                 </div>
             </div>
@@ -100,6 +187,9 @@ const {proxy} = getCurrentInstance()
 const data = reactive({
     orderFilter: '',
     orderList: [],
+    totalPage: 1,
+    selectedPage: 1,
+    searchOid: '',
     orderStatus: {
         '0': '待付款',
         '1': '已付款',
@@ -113,17 +203,20 @@ const data = reactive({
 
 onBeforeRouteUpdate((to, from, next) => {//用于相同路径切换，执行某种事件
     data.orderFilter = to.query.orderFilter
-    getOrderList()
+    getOrderList('',1)
     next()
 })
 onMounted(() => {
     data.orderFilter = proxy.$route.query.orderFilter
-    getOrderList()
+    getOrderList('',1)
 })
 
-function getOrderList() {
-    proxy.$axios.post('/order/orderList', {orderFilter: data.orderFilter}).then(response => {
-        data.orderList = response.data
+function getOrderList(oid,page) {
+    data.searchOid=oid;
+    data.selectedPage=page;
+    proxy.$axios.post('/order/orderList', {oid:data.searchOid,orderFilter: data.orderFilter, page: page}).then(response => {
+        data.orderList = response.data.orderList;
+        data.totalPage = response.data.totalPage;
     })
 }
 </script>
