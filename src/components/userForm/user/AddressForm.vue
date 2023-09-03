9323d781f40bdcc1dad1bc073049e95889d652e1
Teriri <lsy1511736399@outlook.com>
2023-09-03 19:34:39 +0800

    我的地址

--- src/components/userForm/user/AddressForm.vue
+++ src/components/userForm/user/AddressForm.vue
@@ -1,4 +1,42 @@
 <template>
+    <div class="modal fade" id="addressModal" tabindex="-1" aria-labelledby="addressModal" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5">{{data.modalTitle}}</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="form-floating mb-3">
+                        <input type="text" class="form-control" v-model="data.address.receiverName" placeholder="name@example.com">
+                        <label>收货人</label>
+                    </div>
+                    <div class="form-floating mb-3">
+                        <input type="number" class="form-control" v-model="data.address.phone" placeholder="name@example.com">
+                        <label>手机号码</label>
+                    </div>
+                    <div class="form-floating mb-3" data-toggle="distpicker" id="addressForm">
+                        <select class="form-select mb-3" id="province"></select>
+                        <select class="form-select mb-3" id="city"></select>
+                        <select class="form-select mb-3" id="district"></select>
+                    </div>
+                    <div class="form-floating mb-3">
+                        <textarea class="form-control" v-model="data.address.address" placeholder="Leave a comment here"></textarea>
+                        <label>详细地址</label>
+                    </div>
+                    <div class="form-check mb-3" v-show="!data.isAddModal">
+                        <input class="form-check-input" type="checkbox" v-model="data.address.isDefault">
+                        <label class="form-check-label" for="is_default">设为默认地址</label>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">关闭</button>
+                    <button type="button" class="btn btn-primary" v-show="data.isAddModal" @click="addSubmitButton">添加</button>
+                    <button type="button" class="btn btn-primary" v-show="!data.isAddModal" @click="updateSubmitButton">修改</button>
+                </div>
+            </div>
+        </div>
+    </div>
     <div class="container" style="width: 90%">
         <div class="row">
             <HeaderForm/>
@@ -12,7 +50,7 @@
                         <div class="col">
                             <div class="card" style="width: 200px;height: 210px">
                                 <div class="card-body text-center nav flex-column justify-content-center">
-                                    <button type="button" class="btn btn-light" onclick="addAddress()">
+                                    <button type="button" class="btn btn-light" @click="addAddress">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
                                              fill="currentColor"
                                              class="bi bi-plus-circle" viewBox="0 0 16 16">
@@ -24,21 +62,16 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="col">
+                        <div class="col" v-for="address in data.addressList" :key="address">
                             <div class="card" style="width: 200px;height: 210px">
                                 <div class="card-body">
-                                    <h5 class="card-title">${address.receiver_name}</h5>
-                                    <p style="font-size: 3px">${address.phone}</p>
-                                    <p style="font-size: 3px">${address.province} ${address.city}
-                                        ${address.district}<br>${address.address}
-                                    </p>
+                                    <h5 class="card-title" v-if="address.isDefault===1">{{ address.receiverName }}【默认】</h5>
+                                    <h5 class="card-title" v-else>{{ address.receiverName }}</h5>
+                                    <p style="font-size: 3px">{{ address.phone }}</p>
+                                    <p style="font-size: 3px">{{ address.province }} {{ address.city }} {{ address.district }}<br>{{ address.address }}</p>
                                     <div class="btn-group btn-group-sm" role="group" aria-label="button group">
-                                        <button type="button" class="btn btn-outline-dark"
-                                                onclick="updateAddress(${address.toJSONString()})">修改
-                                        </button>
-                                        <button type="button" class="btn btn-outline-dark"
-                                                onclick="openDeleteModal(${address.id})">删除
-                                        </button>
+                                        <button type="button" class="btn btn-outline-dark" @click="updateAddress(address)">修改</button>
+                                        <button type="button" class="btn btn-outline-dark">删除</button>
                                     </div>
                                 </div>
                             </div>
@@ -52,9 +85,62 @@
     </div>
 </template>
 <script setup>
-
 import Utils from "@/components/userForm/Utils.vue";
 import SideMenu from "@/components/userForm/user/SideMenu.vue";
 import FooterForm from "@/components/FooterForm.vue";
 import HeaderForm from "@/components/userForm/HeaderForm.vue";
+import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.min'
+import {getCurrentInstance, onMounted, reactive} from "vue";
+import 'distpicker/dist/distpicker.esm.js'
+import $ from 'jquery/dist/jquery'
+
+let addressModal = null;
+let distPicker = null;
+const {proxy} = getCurrentInstance()
+const data = reactive({
+    addressList: [],
+    address: {},
+    modalTitle:'添加收货地址',
+    isAddModal: true
+})
+
+onMounted(() => {
+    getAddressList()
+    addressModal = new bootstrap.Modal('#addressModal');
+    distPicker = $('#addressForm')
+})
+
+function getAddressList() {
+    proxy.$axios.post('/address/addressList').then(response => {
+        data.addressList = response.data
+    })
+}
+
+function addAddress() {
+    data.modalTitle = '添加收货地址'
+    data.isAddModal = true
+    data.address = {}
+    distPicker.distpicker('destroy')
+    distPicker.distpicker('reset')
+    addressModal.show()
+}
+
+function updateAddress(json) {
+    data.modalTitle = '修改收货地址'
+    data.isAddModal = false
+    data.address = JSON.parse(JSON.stringify(json))
+    data.address.isDefault = json.isDefault === 1;
+    distPicker.distpicker('destroy')
+    distPicker.distpicker({province: json.province, city: json.city, district: json.district});
+    distPicker.distpicker('reset')
+    addressModal.show()
+}
+
+function addSubmitButton() {
+    console.log(data.address)
+}
+
+function updateSubmitButton() {
+    console.log(data.address)
+}
 </script>
